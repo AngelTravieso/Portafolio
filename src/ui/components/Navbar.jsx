5026da6c41ab8900c148092a6b2460dffc6dafe8
AngelTravieso <angeltraviesoc@gmail.com>
2023-07-18 20:51:26 -0400

    ADD - fade animations to navbar

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,17 +1,18 @@
 import { Link } from 'react-router-dom';
+import 'animate.css';
 
 // Navbar
 export const Navbar = () => {
     return(
-        <div className="bg-slate-100 p-5">
+        <div className="bg-slate-50 p-5">
             <div className="flex justify-between items-center">
                 {/* Angel Travieso */}
                 <div className="nav-word flex flex-col items-center">
-                    <p className='text-4xl leading-10 font-medium'>Angel Travieso</p>
-                    <p className='text-lg italic'>Desarrollador de Software</p>
+                    <p className='text-4xl leading-10 font-medium animate__animated animate__fadeInDown'>Angel Travieso</p>
+                    <p className='text-lg italic animate__animated animate__flipInX'>Desarrollador de Software</p>
                 </div>
                 {/* NavLinks */}
-                <div className="nav-links px-2">
+                <div className="nav-links px-2 animate__animated animate__fadeInRight">
                     {/* Inicio */}
                     <Link to='/inicio'
                     className='link mx-1 decoration-sky-700 text-xl py-2 px-5 rounded bg-gradient-to-br from-emerald-400 via-teal-400 to-cyan-400 text-white font-medium'>
