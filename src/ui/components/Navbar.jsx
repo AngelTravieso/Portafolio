0a97f69ebeb5d8f19320a87a0ddac2212924d023
AngelTravieso <angeltraviesoc@gmail.com>
2023-07-18 20:08:33 -0400

    ADD - Navbar buttons

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,18 +1,37 @@
 import { Link } from 'react-router-dom';
 
+// Navbar
 export const Navbar = () => {
-
     return(
-        <>
-    
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-            {/* <Link to='/inicio'>Inicio</Link>
-            <Link to='/sobre_mi'>Sobre Mi</Link>
-            <Link to='/servicios'>Servicios</Link>
-            <Link to='/contacto'>Contacto</Link> */}
-        </>
+        <div className="bg-slate-100 py-5">
+            <div className="flex justify-between">
+                <div className="site-img">
+                    
+                </div>
+                <div className="nav-links px-2">
+                    
+                    <Link to='/inicio'
+                    className='mx-1 decoration-sky-700 text-xl py-2 px-5 rounded bg-gradient-to-br from-emerald-400 via-teal-400 to-cyan-400 text-white font-medium'>
+                        Inicio
+                    </Link>
+
+                    <Link to='/sobre_mi'
+                    className='mx-1 decoration-sky-700 text-xl py-2 px-5 rounded bg-gradient-to-br from-emerald-400 via-teal-400 to-cyan-400 text-white font-medium'>
+                        Sobre Mi
+                    </Link>
+                    <Link to='/contacto'
+                    className='mx-1 decoration-sky-700 text-xl py-2 px-5 rounded bg-gradient-to-br from-emerald-400 via-teal-400 to-cyan-400 text-white font-medium'>
+                        Contacto
+                    </Link>
+                    <Link to='/servicios'
+                    className='mx-1 decoration-sky-700 text-xl py-2 px-5 rounded bg-gradient-to-br from-emerald-400 via-teal-400 to-cyan-400 text-white font-medium'>
+                        Servicios
+                    </Link>
+
+
+                </div>
+            </div>
+        </div>
     )
 
 }
